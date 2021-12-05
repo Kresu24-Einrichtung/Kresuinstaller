8feecdae6f2b81106d97692312bfa96bf565659f
Maxisman25 <63217498+Maxisman25@users.noreply.github.com>
2021-12-05 14:09:14 +0100

    Update minecraft.sh

--- installations/minecraft.sh
+++ installations/minecraft.sh
@@ -13,7 +13,35 @@ if ! [ -x "$(command -v curl)" ]; then
   echo "→ Curl wird für diesen Installer benötgt bitte installiere es mit: `apt install curl -y`"
   exit 1
 fi
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+#Functions
 
+check_if_dir() {
+    echo -n "→ Bitte gebe ein Verzeichnis an:"
+    read -r ndir
+    if [ -d "$ndir" ]; then
+        echo "Verzeichnis gefunden!"
+    else
+        echo "Dieses Verzeichnis existiert nicht Installation abgebrochen!"
+        sleep 2
+        exit 1
+    fi
+}
+
+check_if_java() {
+    if ! [ java -version 2>&1 >/dev/null | grep "java version\|openjdk version" ]; then
+        apt install openjdk-16-jre -y
+        java -version
+        echo "→ Java 16 wurde erfolgreich Installiert! Bitte starte die Installation neu!"
+        exit 1
+    else
+        echo "→ Java 15 ist Installiert!"
+    fi
+}
+
+
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 echo "Minecraft Server Installer:"
 
 echo "[1] - Minecraft Vanilla 1.18"
@@ -25,19 +53,8 @@ read number
 if [ $number -eq 1 ]; then
 echo "Installation wird ausgeführt, dies kann einen moment dauern!"
 sleep 5
-echo "→ Bitte gebe das Verzeichnis an!"
-read ndir
-elif ! [ -d "$ndir" ]; then
-echo "Dieses Verzeichnis existiert nicht Installation abgebrochen!"
-exit 1
-elif [ -d "$ndir" ]; then
-echo "→ Verzeichnis gefunden!"
-elif ! [ java -version 2>&1 >/dev/null | grep "java version\|openjdk version" ]; then
-apt install openjdk-16-jre -y
-java -version
-echo "→ Java 16 wurde erfolgreich Installiert! Bitte starte die Installation neu!"
-exit 1
-else
+check_if_dir()
+check_if_java()
 cd $ndir
 wget https://launcher.mojang.com/v1/objects/3cf24a8694aca6267883b17d934efacc5e44440d/server.jar
 echo "eule=true">> eula.txt
