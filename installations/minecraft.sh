b5f3b38009c7daa2c8f2ebe32389fe9271ef6b75
Maxisman25 <63217498+Maxisman25@users.noreply.github.com>
2021-12-05 15:51:09 +0100

    Update minecraft.sh

--- installations/minecraft.sh
+++ installations/minecraft.sh
@@ -30,19 +30,17 @@ echo "Installation wird ausgeführt, dies kann einen moment dauern! - Bitte beac
 sleep 5
 wget "https://launcher.mojang.com/v1/objects/3cf24a8694aca6267883b17d934efacc5e44440d/server.jar"
 echo "eula=true">> eula.txt
-echo "java -Xmx1024M -Xms1024M -jar server.jar nogui">> start.sh
+echo "screen -S Server java -Xmx1024M -Xms1024M -jar server.jar nogui">> start.sh
 chmod 777 start.sh
-screen -S Server
-./start.sh
+bash start.sh
 echo "Installation abgeschlossen!"
 elif [ $number -eq 2 ]; then
 echo "Installation wird ausgeführt, dies kann einen moment dauern! - Bitte beachte das die Installation in dem Verzeichnis sein muss!"
 sleep 5
 wget "https://download.getbukkit.org/spigot/spigot-1.18.jar"
 echo "eula=true">> eula.txt
-echo "java -Xmx1024M -Xms1024M -jar spigot-1.18.jar nogui">> start.sh
+echo "screen -S Server java -Xmx1024M -Xms1024M -jar spigot-1.18.jar nogui">> start.sh
 chmod 777 start.sh
-screen -S Server
-./start.sh
+bash start.sh
 echo "Installation abgeschlossen!"
 fi
